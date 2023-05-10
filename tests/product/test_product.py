42a8a0b53c3ff90206cb65ac9ec815c3666368e6
Emerson Florencio <emersonflorencio@hotmail.com>
2023-05-10 18:24:46 -0300

    feat: req 01 - correção do test product finnazilado

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -5,8 +5,8 @@ def test_cria_produto():
     mock_id = 1
     mock_nome_do_produto = "PlayStation 5"
     mock_nome_da_empresa = "Sony"
-    mock_data_de_fabricacao = "11/12/2020"
-    mock_data_de_validade = "12/11/2030"
+    mock_data_de_fabricacao = "2020-11-12"
+    mock_data_de_validade = "2030-11-12"
     mock_numero_de_serie = "123456"
     mock_instrucoes_de_armazenamento = "Não colocar em local úmido"
 
