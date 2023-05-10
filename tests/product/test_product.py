4236d7bc1777f0e6e69841ed13b1d52abe52725b
Emerson Florencio <emersonflorencio@hotmail.com>
2023-05-10 17:50:40 -0300

    feat: req 01 - correção do test product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,14 +2,22 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
+    mock_id = 1,
+    mock_nome_do_produto = "PlayStation 5",
+    mock_nome_da_empresa = "Sony",
+    mock_data_de_fabricacao = "2020-11-12",
+    mock_data_de_validade = "2030-11-12",
+    mock_numero_de_serie = "123456",
+    mock_instrucoes_de_armazenamento = "Não colocar em local úmido",
+
     product = Product(
-      id=1,
-      nome_do_produto="PlayStation 5",
-      nome_da_empresa="Sony",
-      data_de_fabricacao="2020-11-12",
-      data_de_validade="2030-11-12",
-      numero_de_serie="123456",
-      instrucoes_de_armazenamento="Não colocar em local úmido",
+        mock_id,
+        mock_nome_do_produto,
+        mock_nome_da_empresa,
+        mock_data_de_fabricacao,
+        mock_data_de_validade,
+        mock_numero_de_serie,
+        mock_instrucoes_de_armazenamento
     )
 
     assert product.id == 1
