4afc11f651c4a257014e0df6590e237d951ca871
Emerson Florencio <emersonflorencio@hotmail.com>
2023-05-13 17:47:35 -0300

    feat: req 03 - implementannto o complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,15 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(lista):
+        report = SimpleReport.generate(lista)
+        report += "\nProdutos estocados por empresa:\n"
+        companies = Counter(item["nome_da_empresa"] for item in lista)
+
+        for product in companies.items():
+            report += f"- {product[0]}: {product[1]}\n"
+
+        return report
