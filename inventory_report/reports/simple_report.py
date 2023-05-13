de699e447517a12d9a9d13a24e41d978555dc5ce
Emerson Florencio <emersonflorencio@hotmail.com>
2023-05-13 17:11:10 -0300

    feat: req 02 - implementação da classe simple report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(lista):
+        currennt_date = datetime.now().strftime("%Y-%m-%d")
+        oldest_date = [date["data_de_fabricacao"] for date in lista]
+        expiry_date = [
+            date["data_de_validade"]
+            for date in lista
+            if date["data_de_validade"] > currennt_date
+        ]
+        companies = [company["nome_da_empresa"] for company in lista]
+        company = Counter(companies).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {min(oldest_date)}\n"
+            f"Data de validade mais próxima: {min(expiry_date)}\n"
+            f"Empresa com mais produtos: { company}"
+        )
